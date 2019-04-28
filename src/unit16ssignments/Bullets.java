0727084c932505ee43095566418c7118af592a33
lynnepdillman <45905485+lynnepdillman@users.noreply.github.com>
2019-04-28 12:48:49 -0700

    Update Bullets.java

--- src/unit16ssignments/Bullets.java
+++ src/unit16ssignments/Bullets.java
@@ -1,7 +1,8 @@
+package StarFighter;
+
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -10,38 +11,40 @@ import javax.imageio.ImageIO;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Bullets
-{
-	private List<Ammo> ammo;
+public class Bullets {
 
-	public Bullets()
-	{
-	}
+    private List<Ammo> ammo;
 
-	public void add(Ammo al)
-	{
-	}
+    public Bullets() {
+        ammo = new ArrayList<Ammo>();
+    }
 
-	//post - draw each Ammo
-	public void drawEmAll( Graphics window )
-	{
-	}
+    public void add(Ammo al) {
+        ammo.add(al);
+    }
 
-	public void moveEmAll()
-	{
-	}
+    //post - draw each Ammo
+    public void drawEmAll(Graphics window) {
+        for (Ammo i : ammo) {
+            i.draw(window);
+        }
+    }
 
-	public void cleanEmUp()
-	{
-	}
+    public void moveEmAll() {
+        for (Ammo i : ammo) {
+            i.move("UP");
+        }
+    }
 
-	public List<Ammo> getList()
-	{
-		return null;
-	}
+    public void cleanEmUp() {
+        ammo.removeAll(ammo);
+    }
 
-	public String toString()
-	{
-		return "";
-	}
+    public List<Ammo> getList() {
+        return ammo;
+    }
+
+    public String toString() {
+        return "";
+    }
 }
