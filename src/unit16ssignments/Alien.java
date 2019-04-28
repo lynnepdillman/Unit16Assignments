b3ffb9c3bebf5dfd5035822e92a54efc34a9b56a
lynnepdillman <45905485+lynnepdillman@users.noreply.github.com>
2019-04-28 12:08:14 -0700

    Update Alien.java

--- src/unit16ssignments/Alien.java
+++ src/unit16ssignments/Alien.java
@@ -1,59 +1,74 @@
+package StarFighter;
+
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
-
+import java.io.File;
+import java.net.URL;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.io.File;
 import javax.imageio.ImageIO;
 
-public class Alien extends MovingThing
-{
-	private int speed;
-	private Image image;
+public class Alien extends MovingThing {
 
-	public Alien()
-	{
-		this(0,0,0);
-	}
+    private int speed;
+    private Image image;
 
-	public Alien(int x, int y)
-	{
-		this(x,y,0);
-	}
+    public Alien() {
+        this(0, 0, 30, 30, 0);
+    }
 
-	public Alien(int x, int y, int s)
-	{
-		super(x, y);
-		speed=s;
-		try
-		{
-			image = ImageIO.read(new File(alien.JPG"));
-		}
-		catch(Exception e)
-		{
-			System.out.println("Houston we have a problem!");
-		}
-	}
+    public Alien(int x, int y) {
+        super(x, y);
+    }
 
-	public void setSpeed(int s)
-	{
-	   speed = s; 
-	}
+    public Alien(int x, int y, int s) {
+        super(x, y);
+        speed = s;
+    }
 
-	public int getSpeed()
-	{
-	   return speed; 
-	}
+    public Alien(int x, int y, int w, int h, int s) {
+        super(x, y, w, h);
+        speed = s;
+        try {
+            URL url = getClass().getResource("/images/alien.jpg");
+            image = ImageIO.read(url);
+        } catch (Exception e) {
+            //feel free to do something here
+        }
+    }
 
-	public void draw( Graphics window )
-	{
-   	window.drawImage(image,getX(),getY(),80,80,null);
-	}
+    public void setSpeed(int s) {
+        //add code
+        speed = s;
+    }
 
-	public String toString()
-	{
-		return "";
-	}
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void move(String direction) {
+        //add code here
+        if (direction.equals("DOWN")) {
+            super.setY(super.getY() + getSpeed());
+        }
+        if (direction.equals("UP")) {
+            super.setY(super.getY() - getSpeed());
+        }
+        if (direction.equals("LEFT")) {
+            super.setX(super.getX() - getSpeed());
+        }
+        if (direction.equals("RIGHT")) {
+            super.setX(super.getX() + getSpeed());
+        }
+    }
+
+    public void draw(Graphics window) {
+        window.drawImage(image, getX(), getY(), getWidth(), getHeight(), null);
+    }
+
+    public String toString() {
+        return "";
+    }
 }
